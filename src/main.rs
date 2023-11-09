38103cdf1f5f11ab3fd03897d37e1cfc93d419f7
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2023-11-09 11:32:37 +0800

    Add json import/export

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
-use std::io::stdin;
+use std::fs::File;
+use std::io::{stdin, Write, self, BufReader};
 use std::collections::{BTreeMap, BTreeSet};
-
-type Graph<'a > = BTreeMap<&'a str, BTreeMap<&'a str, usize>>;
-fn dijakstra<'a>(graph: Graph<'a>, start: &'a str, goal: &'a str) -> Result<Vec<&'a str>, &'a str> {
+type Graph<T> = BTreeMap<T, BTreeMap<T, usize>>;
+fn dijakstra<'a>(graph: &'a Graph<String>, start: &'a str, goal: &'a str) -> Result<Vec<&'a str>, &'a str> {
     let mut open_list: BTreeSet<(usize, &str)> = BTreeSet::new();
-    open_list.insert((0, start));
+    open_list.insert((0, &start));
 
-    // Assign to every node a tentative distance value
-    // infinity for all other nodes
-    let mut distances: BTreeMap<&str, usize> = graph.keys().map(|k|{ (*k, usize::MAX) }).collect();
+    // Assign to every node a tentative distance value of infinity
+    let mut distances: BTreeMap<&str, usize> = graph.keys().map(|k|{ (k.as_str(), usize::MAX) }).collect();
     // set it to zero for start node
-    distances.insert(start, 0);
+    distances.insert(&start, 0);
 
     let mut origin: BTreeMap<&str, &str> = BTreeMap::new();
 
@@ -19,11 +18,11 @@ fn dijakstra<'a>(graph: Graph<'a>, start: &'a str, goal: &'a str) -> Result<Vec<
 
         if current == goal  {
             // backtrack to reconstruct the path
-            let mut path:Vec<&'a str> = vec![goal];
+            let mut path:Vec<&'a str> = vec![&goal];
             let mut node = goal;
-            while let Some(next) = origin.get(node) {
+            while let Some(next) = origin.get(&node) {
                 node = next;
-                path.push(node);
+                path.push(&node);
             }
          
             path.reverse();
@@ -31,11 +30,11 @@ fn dijakstra<'a>(graph: Graph<'a>, start: &'a str, goal: &'a str) -> Result<Vec<
         }
 
         // Visit each neighbor of the current node
-        for (neighbor, cost) in graph.get(&current).unwrap() {
+        for (neighbor, cost) in graph.get(current).unwrap() {
             let new_distance = distances.get(current).unwrap() + cost;
-            // Compare the new distance with the previous ones
+            // Compare the new distance with the cost in distances
             // keep the least distance
-            if new_distance < *distances.get(neighbor).unwrap() {
+            if new_distance < *distances.get(neighbor.as_str()).unwrap() {
                 origin.insert(neighbor, current);
                 distances.insert(neighbor, new_distance);
                 open_list.insert((new_distance, neighbor));
@@ -46,11 +45,16 @@ fn dijakstra<'a>(graph: Graph<'a>, start: &'a str, goal: &'a str) -> Result<Vec<
     Err("No path found")
 }
 
-fn ask_node(graph: &Graph, msg: &str) -> String {
+// Helper function to get user input
+fn input() -> String {
+    let mut mode: String = String::new();
+    stdin().read_line(&mut mode).unwrap();
+    mode.trim().to_string()
+}
+
+fn ask_node(graph: &Graph<String>, msg: &str) -> String {
     println!("{}", msg);
-    let mut node: String = String::new();
-    stdin().read_line(&mut node).unwrap();
-    node = node.trim().to_string();
+    let node = input();
     if graph.contains_key(node.as_str()) {
         return node;
     }
@@ -58,7 +62,7 @@ fn ask_node(graph: &Graph, msg: &str) -> String {
     ask_node(graph, msg)
 }
 
-fn show_nodes(graph: &Graph) {
+fn show_nodes(graph: &Graph<String>) {
     println!("Nodes:");
     for key in graph.keys() {
         print!("{} ", key);
@@ -66,47 +70,20 @@ fn show_nodes(graph: &Graph) {
     println!();
 }
 
-fn main() {
-//     A
-//    / \
-//   B - C
-//  / \ / \
-//  |  D   F
-//  \  /
-//   E
-// Define a graph as an adjacency list
-    let graph: Graph = BTreeMap::from(
-        [
-            ("A", BTreeMap::from([
-                ("B", 3),
-                ("C", 5),
-            ])),
-            ("B", BTreeMap::from([
-                ("A", 3),
-                ("C", 2),
-                ("D", 6),
-                ("E", 4),
-            ])),
-            ("C", BTreeMap::from([
-                ("B", 2),
-                ("A", 5),
-                ("D", 1),
-                ("F", 8),
-            ])),
-            ("D", BTreeMap::from([
-                ("B", 6),
-                ("C", 1),
-                ("E", 4),
-            ])),
-            ("E", BTreeMap::from([
-                ("B", 4),
-                ("D", 4),
-            ])),
-            ("F", BTreeMap::from([
-                ("C", 8),
-            ])),
-        ]
-    );
+fn export_json(graph: &Graph<String>, filepath: &str) {
+    let json = serde_json::to_string_pretty(graph).unwrap();
+    let mut file = File::create(filepath).unwrap();
+    file.write_all(json.as_bytes()).unwrap();
+    println!("Graph exported to {}", filepath);
+}
+
+fn import_json(filepath: &str) -> Graph<String> {
+    let file = File::open(filepath).unwrap();
+    let reader = BufReader::new(file);
+    serde_json::from_reader(reader).unwrap()
+}
+
+fn exec(graph: &Graph<String>) {
     println!("Dijakstra's Algorithm");
     println!("Graph: {:#?}", graph);
     // Ask user for start and goal node
@@ -115,7 +92,7 @@ fn main() {
     println!();
     show_nodes(&graph);
     let goal = ask_node(&graph, "Input GOAL node:");
-    if let Ok(path ) = dijakstra(graph, &start, &goal) {
+    if let Ok(path ) = dijakstra(&graph, &start, &goal) {
         let output = path.join(" -> ");
         println!("Least Cost Path: \n{}",output );
     } else {
@@ -124,10 +101,89 @@ fn main() {
 
     // prompt whether to repeat
     println!("continue? (y/n)");
-    let mut repeat: String = String::new();
-    stdin().read_line(&mut repeat).unwrap();
-    repeat = repeat.trim().to_string();
+    let repeat = input();
     if repeat == "y" {
         main();
     }
+}
+
+fn get_graph_source() -> Graph<String> {
+    print!("Choose graph source:\n[0] Use sample graph\n[1] Import json file\n>> ");
+    io::stdout().flush().unwrap();
+    let mode = input();
+    match mode.as_str() {
+        "0" => {
+            //     A
+            //    / \
+            //   B - C
+            //  / \ / \
+            //  |  D   F
+            //  \  /
+            //   E
+            // Define a graph as an adjacency list
+            BTreeMap::from(
+                [
+                    ("A".to_owned(), BTreeMap::from([
+                        ("B".to_owned(), 3),
+                        ("C".to_owned(), 5),
+                    ])),
+                    ("B".to_owned(), BTreeMap::from([
+                        ("A".to_owned(), 3),
+                        ("C".to_owned(), 2),
+                        ("D".to_owned(), 6),
+                        ("E".to_owned(), 4),
+                    ])),
+                    ("C".to_owned(), BTreeMap::from([
+                        ("B".to_owned(), 2),
+                        ("A".to_owned(), 5),
+                        ("D".to_owned(), 1),
+                        ("F".to_owned(), 8),
+                    ])),
+                    ("D".to_owned(), BTreeMap::from([
+                        ("B".to_owned(), 6),
+                        ("C".to_owned(), 1),
+                        ("E".to_owned(), 4),
+                    ])),
+                    ("E".to_owned(), BTreeMap::from([
+                        ("B".to_owned(), 4),
+                        ("D".to_owned(), 4),
+                    ])),
+                    ("F".to_owned(), BTreeMap::from([
+                        ("C".to_owned(), 8),
+                    ])),
+                ]
+            )
+        },
+        "1" => {
+            println!("Enter filepath:");
+            let filepath = input();
+            import_json(filepath.as_str())
+        },
+        _ => {
+            println!("Invalid mode");
+            get_graph_source()
+        }
+    }
+}
+fn main() {
+    // prompt whether to import graph, use sample, or input graph
+    let graph: Graph<String> = get_graph_source();
+    // prompt whether to export graph or show dijakstra
+    print!("Choose mode:\n[0] Export graph\n[1] Show dijakstra\n>> ");
+    io::stdout().flush().unwrap();
+    let mode = input();
+    match mode.as_str() {
+        "0" => {
+            println!("Input filepath:");
+            let filepath = input();
+            export_json(&graph, filepath.as_str());
+        },
+        "1" => {
+            exec(&graph);
+        },
+        _ => {
+            println!("Invalid mode");
+            main();
+        }
+    };
 }
